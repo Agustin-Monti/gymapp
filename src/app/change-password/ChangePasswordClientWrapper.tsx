0055f6f8cfb245a792f71b1dd52636fb575fcc3c
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-05-29 11:15:53 -0300

    Create ChangePasswordClientWrapper.tsx

---
+++ src/app/change-password/ChangePasswordClientWrapper.tsx
@@ -0,0 +1,8 @@
+// src/app/change-password/ChangePasswordClientWrapper.tsx  (Client Component)
+'use client';
+
+import ChangePasswordClient from '@/components/ChangePasswordClient';
+
+export default function ChangePasswordClientWrapper() {
+  return <ChangePasswordClient />;
+}
