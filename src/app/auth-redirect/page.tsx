6acd8b73124f1180bc19f35ac4e3446868755ee4
Agustin-Monti <153923960+Agustin-Monti@users.noreply.github.com>
2025-06-10 13:58:35 -0300

    Update page.tsx

--- src/app/auth-redirect/page.tsx
+++ src/app/auth-redirect/page.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import AuthRedirectClient from '../../components/AuthRedirectClient';
 
 export default function Page() {
   return <AuthRedirectClient />;
 }
-
